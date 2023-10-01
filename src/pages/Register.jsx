1fa66c4a14226cf1681f7980dd2903aa22636781
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-01 16:32:39 +0200

    Add register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,29 @@
+import { FormInput, SubmitBtn } from '../components';
+import { Form, Link } from 'react-router-dom';
+
+const Register = () => {
+  return (
+    <section className='h-screen grid place-content-center'>
+      <Form
+        method='post'
+        className='card w-96 sm:w-[600px] p-8 bg-base-100 shadow-lg flex flex-col gap-y-4'
+      >
+        <h4 className='text-center text-3xl font-bold'>register</h4>
+        <FormInput type='text' label='username' name='username' />
+        <FormInput type='email' label='email' name='email' />
+        <FormInput type='password' label='password' name='password' />
+        <div className='mt-4'>
+          <SubmitBtn text='register' />
+        </div>
+        <p className='text-center'>
+          Already a member?
+          <Link to='/login' className='ml-2 link-hover link-primary capitalize'>
+            login
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
+};
+
+export default Register;
