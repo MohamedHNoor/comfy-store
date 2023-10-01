4513ad62851f95732748d8b240736734767f17e8
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-01 16:31:51 +0200

    Add login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,44 @@
+import { FormInput, SubmitBtn } from '../components';
+import { Form, Link } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <section className='h-screen grid place-content-center'>
+      <Form
+        method='post'
+        className='card w-96 sm:w-[600px] p-8 bg-base-100 shadow-lg flex flex-col gap-y-4'
+      >
+        <h4 className='text-center text-3xl font-bold'>login</h4>
+        <FormInput
+          type='email'
+          label='email'
+          name='identifier'
+          defaultValue='test@test.com'
+        />
+        <FormInput
+          type='password'
+          label='password'
+          name='identifier'
+          defaultValue='secret'
+        />
+        <div className='mt-4'>
+          <SubmitBtn text='login' />
+        </div>
+        <button type='button' className='btn btn-secondary btn-block'>
+          guest user
+        </button>
+        <p className='text-center'>
+          Not a member yet?{' '}
+          <Link
+            to='/register'
+            className='ml-2 link-hover link-primary capitalize'
+          >
+            register
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
+};
+
+export default Login;
