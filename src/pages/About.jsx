cd34269a6e5d19ef292a855eb0c14ee945e7ef25
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-02 12:57:53 +0200

    Add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,25 @@
 const About = () => {
-  return <h1 className='text-4xl'>About page</h1>;
+  return (
+    <>
+      <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
+        <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+          we love
+        </h1>
+        <div className='stats bg-primary shadow'>
+          <div className='stat'>
+            <div className='stat-title text-primary-content text-4xl font-bold tracking-widest'>
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className='max-w-2xl mt-6 leading-8 mx-auto text-lg'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem sint et
+        similique molestias repellendus illo distinctio hic maxime placeat quia.
+        At, quam dolores architecto sunt laudantium ad! Quibusdam, mollitia cum?
+      </p>
+    </>
+  );
 };
 
 export default About;
