731c9498bda70f77bc813f6dec0353a67797921a
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-07 23:06:42 +0200

    Completed filter component functionality

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,7 +5,8 @@ import FormRange from './FormRange';
 import FormCheckbox from './FormCheckbox';
 
 const Filters = () => {
-  const { meta } = useLoaderData();
+  const { meta, params } = useLoaderData();
+  const { search, company, category, shipping, order, price } = params;
   return (
     <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  items-center'>
       {/* SEARCH */}
@@ -14,6 +15,7 @@ const Filters = () => {
         label='search product'
         name='search'
         size='input-sm'
+        defaultValue={search}
       />
       {/* CATEGORIES */}
       <FormSelect
@@ -21,6 +23,7 @@ const Filters = () => {
         name='category'
         list={meta.categories}
         size='select-sm'
+        defaultValue={category}
       />
       {/* COMPANIES */}
       <FormSelect
@@ -28,6 +31,7 @@ const Filters = () => {
         name='company'
         list={meta.companies}
         size='select-sm'
+        defaultValue={company}
       />
       {/* ORDER */}
       <FormSelect
@@ -35,11 +39,22 @@ const Filters = () => {
         name='order'
         list={['a-z', 'z-a', 'high', 'low']}
         size='select-sm'
+        defaultValue={order}
       />
       {/* PRICE */}
-      <FormRange name='price' label='select price' size='range-sm' />
+      <FormRange
+        name='price'
+        label='select price'
+        size='range-sm'
+        price={price}
+      />
       {/* SHIPPING */}
-      <FormCheckbox name='shipping' label='free shipping' size='checkbox-sm' />
+      <FormCheckbox
+        name='shipping'
+        label='free shipping'
+        size='checkbox-sm'
+        defaultValue={shipping}
+      />
       {/* BUTTONS */}
       <button type='submit' className='btn btn-primary btn-sm'>
         submit
