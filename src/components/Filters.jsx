be9787a06eab452ad9e9412c22142a14ac6af66b
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-07 22:30:55 +0200

    Create filter component

---
+++ src/components/Filters.jsx
@@ -0,0 +1,54 @@
+import { Form, useLoaderData, Link } from 'react-router-dom';
+import FormInput from './FormInput';
+import FormSelect from './FormSelect';
+import FormRange from './FormRange';
+import FormCheckbox from './FormCheckbox';
+
+const Filters = () => {
+  const { meta } = useLoaderData();
+  return (
+    <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  items-center'>
+      {/* SEARCH */}
+      <FormInput
+        type='search'
+        label='search product'
+        name='search'
+        size='input-sm'
+      />
+      {/* CATEGORIES */}
+      <FormSelect
+        label='select category'
+        name='category'
+        list={meta.categories}
+        size='select-sm'
+      />
+      {/* COMPANIES */}
+      <FormSelect
+        label='select company'
+        name='company'
+        list={meta.companies}
+        size='select-sm'
+      />
+      {/* ORDER */}
+      <FormSelect
+        label='sort by'
+        name='order'
+        list={['a-z', 'z-a', 'high', 'low']}
+        size='select-sm'
+      />
+      {/* PRICE */}
+      <FormRange name='price' label='select price' size='range-sm' />
+      {/* SHIPPING */}
+      <FormCheckbox name='shipping' label='free shipping' size='checkbox-sm' />
+      {/* BUTTONS */}
+      <button type='submit' className='btn btn-primary btn-sm'>
+        submit
+      </button>
+      <Link to='/products' className='btn btn-accent btn-sm'>
+        reset
+      </Link>
+    </Form>
+  );
+};
+
+export default Filters;
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const FormInput = ({ label, name, type, defaultValue }) => {
+const FormInput = ({ label, name, type, defaultValue, size }) => {
   return (
     <div className='form-control'>
       <label className='label'>
-        <span className='label-text'>{label}</span>
+        <span className='label-text capitalize'>{label}</span>
       </label>
       <input
         type={type}
         name={name}
         defaultValue={defaultValue}
-        className='input input-bordered'
+        className={`input input-bordered ${size}`}
       />
     </div>
   );
