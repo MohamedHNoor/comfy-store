9fd3995e4a1729d9cd65a1d1323cdba48ac869b2
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-13 15:30:38 +0200

    Create register functionality

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,31 @@
 import { FormInput, SubmitBtn } from '../components';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, redirect } from 'react-router-dom';
+import { customFetch } from '../utils';
+import { toast } from 'react-toastify';
 
-export const action = async () => {
-  return null;
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    const response = await customFetch.post('/auth/local/register', data);
+    console.log(response);
+    toast.success('Account created successfully');
+    return redirect('/login');
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data?.error?.message ||
+      'Please double check your credentials';
+    toast.error(errorMessage);
+    return null;
+  }
 };
 
 const Register = () => {
   return (
     <section className='h-screen grid place-content-center'>
       <Form
-        method='post'
+        method='POST'
         className='card w-96 sm:w-[600px] p-8 bg-base-100 shadow-lg flex flex-col gap-y-4'
       >
         <h4 className='text-center text-3xl font-bold'>Register</h4>
