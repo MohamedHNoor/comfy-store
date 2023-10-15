6528215650ee1494f03c1ca89cd299d0458f38d6
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-15 14:35:06 +0200

    Add pagination to order page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,11 @@
 import { redirect, useLoaderData } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { customFetch } from '../utils';
-import { OrdersList, PaginationContainer, SectionTitle } from '../components';
+import {
+  OrdersList,
+  ComplexPaginationContainer,
+  SectionTitle,
+} from '../components';
 
 export const loader =
   (store) =>
@@ -45,7 +49,7 @@ const Orders = () => {
     <>
       <SectionTitle text='Your Order' />
       <OrdersList />
-      <PaginationContainer />
+      <ComplexPaginationContainer />
     </>
   );
 };
