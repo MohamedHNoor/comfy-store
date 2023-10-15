a7e931ae3704cdf0dbc1431e928fb81b90697a7b
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-15 13:36:17 +0200

    Add orders page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import { loader as landingLoader } from './pages/Landing';
 import { loader as SingleProductLoader } from './pages/SingleProduct';
 import { loader as productsLoader } from './pages/Products';
 import { loader as checkoutLoader } from './pages/Checkout';
+import { loader as orderLoader } from './pages/Orders';
 
 // actions
 import { action as registerAction } from './pages/Register';
@@ -62,6 +63,7 @@ const router = createBrowserRouter([
       {
         path: 'orders',
         element: <Orders />,
+        loader: orderLoader(store),
       },
       {
         path: 'products/:id',
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,5 +1,53 @@
+import { redirect, useLoaderData } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { customFetch } from '../utils';
+import { OrdersList, PaginationContainer, SectionTitle } from '../components';
+
+export const loader =
+  (store) =>
+  async ({ request }) => {
+    const user = store.getState().userState.user;
+
+    if (!user) {
+      toast.warning('You must be logged in to view the orders');
+      return redirect('/login');
+    }
+
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+
+    try {
+      const response = await customFetch.get('/orders', {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      console.log(response);
+      return { orders: response.data.data, meta: response.data.meta };
+    } catch (error) {
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        'There was an error placing your order';
+      toast.error(errorMessage);
+      if (error.response.status === 401 || 403) return redirect('/login');
+      return null;
+    }
+  };
+
 const Orders = () => {
-  return <h1 className='text-4xl'>Orders</h1>;
+  const { meta } = useLoaderData();
+  if (meta.pagination.total < 1) {
+    return <SectionTitle text='Please make an order' />;
+  }
+  return (
+    <>
+      <SectionTitle text='Your Order' />
+      <OrdersList />
+      <PaginationContainer />
+    </>
+  );
 };
 
 export default Orders;
